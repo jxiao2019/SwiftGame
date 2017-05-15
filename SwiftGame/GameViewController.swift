5031dbbdbed0fb3fb3439832db714313af48fac7
jxiao2019 <jxiao2019@pingry.org>
2017-05-15 10:20:09 -0400

    Version 2 (With some of the deleted code)
    Refer to previous commit if want the starter code for Swift’s own game

--- SwiftGame/GameViewController.swift
+++ SwiftGame/GameViewController.swift
@@ -18,46 +18,19 @@ class GameViewController: UIViewController {
     
     
     
+    @IBAction func play(_ sender: Any) {
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
     }
-
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+    
+    override var shouldAutorotate: Bool {
+        return false
     }
 }
